3127fb2f749c10f463154d8cbb295f45caadf67a
Andri Juni <32693632+andrijuni@users.noreply.github.com>
2019-07-23 17:44:15 +0700

    Memperbarui!

---
+++ DevC-Exercise4-master/js/function.js
@@ -0,0 +1,3 @@
+function about() {
+  window.location = "about.html";
+};
---
+++ DevC-Exercise4-master/js/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $("#navb").load("navbar.html");
+    $("#slider").load("slider.html");
+    $("#modal").load("modal.html");
+    $("#footer").load("footer.html");
+});
